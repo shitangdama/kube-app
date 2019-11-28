8b1dd2be8e63aa11e1ab60114e0c0962012d39f0
jadertao <jader_tao@qq.com>
2019-11-29 00:14:15 +0800

    feat(编辑器): monaco

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const prod = process.env.NODE_ENV === 'production'
 
 const http_api = {
-  'SERVER_URL': prod ? 'https://laki.shitangdama.cn' :'http://127.0.0.1:8080',
-  'CLIENT_URL': prod ? 'http://laki-api.shitangdama.cn' :'http://127.0.0.1:3000',
+  'SERVER_URL': prod ? 'https://laki-api.shitangdama.cn' : 'https://laki-api.shitangdama.cn',
+  'CLIENT_URL': prod ? 'https://laki.shitangdama.cn' : 'http://127.0.0.1:3000',
 }
 
 export default http_api;
--- src/pages/deployment/edit.js
+++ src/pages/deployment/edit.js
@@ -1,49 +1,73 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-
-// import MonacoEdit from "../../components/codeEdit"
-import AceEditor from "react-ace";
-
-import "ace-builds/src-noconflict/theme-github";
+import MonacoEditor, { MonacoEditorProps } from 'react-monaco-editor';
+import ResizeObserver from 'resize-observer-polyfill';
+import debounce from 'lodash/debounce';
 
 import {
     Card
 } from 'antd';
+import './edit.less';
 
 @inject('deploy')
 @observer
 export default class Edit extends React.Component {
+    handleResize = debounce(([entry]) => {
+        console.log(entry.contentRect)
+        this.setState({
+            height: Math.max(entry.contentRect.height, this.state.containerHeight),
+            width: entry.contentRect.width,
+        })
+    }, 100)
 
     state = {
         code: "",
-        language: "json"
-
+        language: "json",
+        height: 500,
+        width: 100,
+        containerHeight: 500,
     }
+
+    observer = null
+    container = React.createRef()
 
     componentDidMount() {
+        this.observer = new ResizeObserver(this.handleResize)
+        if (this.container.current) {
+            this.observer.observe(this.container.current)
+        }
         this.init()
     }
+
+    componentWillUnmount() {
+        if (this.observer) {
+            this.observer.disconnect()
+        }
+    }
+
 
     init = async () => {
         const name = this.props.match.params.name
         const data = await this.props.deploy.getDeploy(name)
         this.setState({
-            code: JSON.stringify(data,null, 4)
+            code: JSON.stringify(data, null, 4),
+        })
+        setTimeout(() => {
+            this.setState({
+                containerHeight: document.querySelector('main .container').getBoundingClientRect().height,
+            })
         })
     }
 
+    handleCodeChange = debounce(() => {
+    }, 200)
+
     render() {
+        const { width, height } = this.state
+        const { width: w, height: h, className, ...restProps } = this.props
         return (
-            <div className="card-info-table">
-                <Card bordered={false}>
-                    <AceEditor
-                        mode="json"
-                        theme="github"
-                        value={this.state.code}
-                        onChange={this.onEditChange}
-                        name="editapp"
-                    />
-                </Card>
+            <div className={className} style={{ height: '100%', width: '100%' }} ref={this.container}>
+                <MonacoEditor width={width} height={height} value={this.state.code} {...restProps} onChange={this.handleCodeChange} />
             </div>
         )
     }
